d6915ec90c0376afafefee7c61a38836a348e0b4
kavya1303 <kavyagupta1303@gmail.com>
2022-05-15 00:02:35 +0530

    added movieapp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+import Movies from "./components/Movies";
+import Favourite from "./components/Favourite";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <h1>Movies App</h1>
+    <BrowserRouter>
+      <NavBar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner /> 
+              <Movies />
+            </>
+          }
+        />
+        <Route path="/favourites" element={<Favourite />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
